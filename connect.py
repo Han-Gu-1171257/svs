3f4170f07a898413d987a594ff9eda19468786ad
HanGu1171257 <Han.Gu@lincolnuni.ac.nz>
2025-10-11 18:10:51 +1300

    chore: ignore connect.py and stop tracking it

--- connect.py
+++
@@ -1,7 +0,0 @@
-# Database connection configuration
-dbuser = "root"
-dbpass = "your_password_here"   # <-- Enter your MySQL Server password here 
-                                # (NOT PRIVATE - don't re-use an important password you use elsewhere)
-dbhost = "127.0.0.1"
-dbport = 3306
-dbname = "svs"
